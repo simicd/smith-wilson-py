87883505f5a93a0b8b323ed8958b4b57e9110b35
Dejan Simic <10134699+simicd@users.noreply.github.com>
2022-01-23 15:29:52 +0100

    Create python-package.yml

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="smithwilson",
     version="0.2.0",
@@ -14,8 +17,13 @@ setuptools.setup(
     url="https://github.com/simicd/smith-wilson-py.git",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.7',
+    install_requires=required,
 )
