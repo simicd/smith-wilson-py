022d868e6b14cb4656c8b96d26e68f58cf9ab391
Dejan Simic <10134699+simicd@users.noreply.github.com>
2022-01-25 21:13:03 +0100

    Fix installation requirement

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setuptools.setup(
     name="smithwilson",
     version="0.2.0",
     author="Dejan Simic",
     author_email="dejan.simic",
-    description="Implementation of the Smith-Wilson yield curve fitting algorithm in Python for interpolations and extrapolations of zero-coupon bond rates",
+    description=
+    "Implementation of the Smith-Wilson yield curve fitting algorithm in Python for interpolations and extrapolations of zero-coupon bond rates",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/simicd/smith-wilson-py.git",
@@ -25,5 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
-    install_requires=required,
+    install_requires=["numpy>=1.21.5"],
 )
