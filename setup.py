0722088b0a4b605674f280c31eb3486ed2e2a845
Dejan Simic <10134699+simicd@users.noreply.github.com>
2022-01-27 21:16:11 +0100

    Combine CI workflows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="smithwilson",
-    version="0.2.1",
+    version="0.2.2",
     author="Dejan Simic",
     # author_email="dejan.simic",
     description=
